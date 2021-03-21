d87fd72551e5e9f6b1a62da355cdc2946c7c0f8e
Roman Zakharenkov <roman.zakharenkov@gmail.com>
2021-03-21 15:11:41 +0200

    Added LESS, PostCSS, terser.

---
+++ snowpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
+  },
+  plugins: [
+    "@snowpack/plugin-postcss",
+    "snowpack-plugin-less",
+    [
+      'snowpack-plugin-terser',
+      {
+        /**
+         * @see Plugin Options below
+         */
+        terserOptions: {
+          compress: {
+            arguments: true,
+            passes: 2,
+            unsafe_arrows: true,
+          },
+        },
+      },
+    ],
+  ],
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    bundle: true
+  },
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
